bbe9c032e5f61ebfb5e59c41d71ed82d1065b89b
Abinav Seelan <abinav.n.seelan@gmail.com>
2017-09-10 16:21:19 +0530

    Add debounce to Comment

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,55 @@
 
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { debounce } from 'throttle-debounce';
+
+import styles from './index.scss';
 
 class App extends Component {
   constructor(args) {
     super(args);
+
+    this.state = {
+      currentText: '',
+      lastSyncedText: '',
+    };
+
+    this.syncServer = debounce(500, this.syncServer.bind(this));
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  syncServer() {
+    console.log(this.state.currentText);
+  }
+
+  handleInput(event) {
+    const text = event.target.value;
+    this.setState({
+      currentText: text,
+    }, () => {
+      this.syncServer();
+    });
+  }
+
+  handleSubmit() {
+    console.log(this.state.currentText);
   }
 
   render() {
     return (
-      <h1>hey</h1>
+      <div className={styles.container}>
+        <textarea
+          className={styles['comments-container']}
+          placeholder="Write your response..."
+          onChange={this.handleInput}
+        />
+        <div className={styles.cta}>
+          <button className={styles['post-btn']} onClick={this.handleSubmit}>
+            Post Comment
+          </button>
+        </div>
+      </div>
     );
   }
 }
